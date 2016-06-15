483d37d1c02309c6617da8f56e85af19a5b29428
Nathan P. Keene <npk@txtincorporated.com>
2016-06-14 17:35:16 -0700

    Article.allAuthors

--- scripts/article.js
+++ scripts/article.js
@@ -73,7 +73,13 @@
   Article.allAuthors = function() {
     // Read docs on .map and .reduce! You can reference the previous
     // `map` in the numWordsAll method to get started here.
-
+    return Article.all.map(function(article) {
+      return article.author;
+    })
+    .reduce(function(a,b) {
+      if (a.indexOf(b) < 0) a.push(b);
+      return a;
+    },[]);
     // For our `reduce` -- since we are trying to return an array, we'll need to specify an accumulator type...
     // what data type should this accumulator be and where is it placed?
     return whatShouldIReturn;
